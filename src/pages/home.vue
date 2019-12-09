cd805435916bf74d7fb6c9bcffdbd94ecde162c3
MySelf <1341933031@qq.com>
2019-12-09 23:19:53 +0800

    初始化

---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+    this is hdn-home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hdn-home',
+  props: {}
+}
+</script>
+
---
+++ src/pages/technology.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello">
+    this is hdn-technology
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hdn-technology',
+  props: {}
+}
+</script>
