1706a3ef4f3653d3ca3c7cd1677e08d0a30cf854
MySelf <1341933031@qq.com>
2019-12-19 00:07:11 +0800

    页面修改

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,20 @@
 <template>
   <el-row :gutter="10">
-      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
-      <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :xs="4" :sm="4" :md="4" :lg="4" :xl="4"><div class="grid-content bg-purple">
+
+      </div></el-col>
+      <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12"><div class="grid-content bg-purple-light">
+          <el-row>
+            <el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">logo</el-col>
+            <el-col :xs="18" :sm="18" :md="18" :lg="18" :xl="18">列表</el-col>
+          </el-row>
+        </div></el-col>
+      <el-col :xs="4" :sm="4" :md="8" :lg="4" :xl="4"><div class="grid-content bg-purple">
+        
+        </div></el-col>
+      <el-col :xs="4" :sm="4" :md="4" :lg="4" :xl="4"><div class="grid-content bg-purple-light">
+        
+        </div></el-col>
   </el-row>
 </template>
 
